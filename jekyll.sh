612e13bcba3f1f669c14ce039b959049b5569795
Кирилл Катровский <katrovskykirill@gmail.com>
2024-01-10 02:34:03 +0400

    Update jekyll.sh
    Оптимизировано, но установка производится только в Arch Linux.

--- jekyll.sh
+++ jekyll.sh
@@ -1,51 +1,46 @@
 #!/bin/bash
 
-install_ruby_fedora() {
-    echo "Установка Ruby для Linux (Fedora)"
-    sudo dnf install ruby ruby-devel openssl-devel redhat-rpm-config gcc-c++ @development-tools
-}
-
-install_ruby_debian() {
-    echo "Установка Ruby для Linux (Debian/Ubuntu)"
-    sudo apt-get update
-    sudo apt-get install -y ruby-full build-essential
-}
-
-install_ruby_opensuse() {
-    echo "Установка Ruby для Linux (OpenSUSE)"
-    sudo zypper install -y -t pattern devel_ruby devel_C_C++
-    sudo zypper install -y ruby-devel
-}
-
 install_ruby_arch() {
     echo "Установка Ruby для Linux (Arch Linux)"
-    sudo pacman -Syu --needed --noconfirm ruby base-devel
-}
-
-install_ruby_clear() {
-    echo "Установка Ruby для Linux (Clear Linux)"
-    sudo swupd update
-    sudo swupd bundle-add-y ruby-basic
+    sudo pacman -Syu --needed --noconfirm ruby base-devel rubygems
 }
 
 configure_ruby_gems_bash() {
-    echo '# Установка Ruby Gems в ~/.gems' >> ~/.bashrc
-    echo 'export GEM_HOME="$HOME/.gems"' >> ~/.bashrc
-    echo 'export PATH="$HOME/.gems/bin:$PATH"' >> ~/.bashrc
+    if ! grep -q 'Установка Ruby Gems в ~/.gems' ~/.bashrc; then
+        cat << 'EOF' >> ~/.bashrc
+# Установка Ruby Gems в ~/.gems
+if [[ ":$PATH:" != *":$HOME/.gems/bin:"* ]]; then
+    export GEM_HOME="$HOME/.gems"
+    export PATH="$HOME/.gems/bin:$PATH"
+fi
+EOF
+    fi
     source ~/.bashrc
 }
 
 configure_ruby_gems_fish() {
-    echo '# Установка Ruby Gems в ~/.gems' >> ~/.config/fish/config.fish
-    echo 'set -x GEM_HOME "$HOME/.gems"' >> ~/.config/fish/config.fish
-    echo 'set -x PATH "$HOME/.gems/bin" $PATH' >> ~/.config/fish/config.fish
+    if ! grep -q 'Установка Ruby Gems в ~/.gems' ~/.config/fish/config.fish; then
+        cat << 'EOF' >> ~/.config/fish/config.fish
+# Установка Ruby Gems в ~/.gems
+if not contains $PATH $HOME/.gems/bin
+    set -x GEM_HOME "$HOME/.gems"
+    set -x PATH "$HOME/.gems/bin" $PATH
+end
+EOF
+    fi
     source ~/.config/fish/config.fish
 }
 
 configure_ruby_gems_zsh() {
-    echo '# Установка Ruby Gems в ~/.gems' >> ~/.zshrc
-    echo 'export GEM_HOME="$HOME/.gems"' >> ~/.zshrc
-    echo 'export PATH="$HOME/.gems/bin:$PATH"' >> ~/.zshrc
+    if ! grep -q 'Установка Ruby Gems в ~/.gems' ~/.zshrc; then
+        cat << 'EOF' >> ~/.zshrc
+# Установка Ruby Gems в ~/.gems
+if [[ ":$PATH:" != *":$HOME/.gems/bin:"* ]]; then
+    export GEM_HOME="$HOME/.gems"
+    export PATH="$HOME/.gems/bin:$PATH"
+fi
+EOF
+    fi
     source ~/.zshrc
 }
 
@@ -53,35 +48,22 @@ install_jekyll_bundler() {
     gem install jekyll bundler
 }
 
-# Определение операционной системы
-os=$(lsb_release -a | awk -F': ' '/Distributor ID/{print $2}')
-
-# Установка Ruby для Linux
-declare -A os_install_functions=(
-    ["Fedora"]="install_ruby_fedora"
-    ["Debian"]="install_ruby_debian"
-    ["Ubuntu"]="install_ruby_debian"
-    ["openSUSE"]="install_ruby_opensuse"
-    ["Arch Linux"]="install_ruby_arch"
-    ["Clear Linux"]="install_ruby_clear"
-)
-
-if [[ -n "${os_install_functions[$os]}" ]]; then
-    ${os_install_functions[$os]}
-fi
-
-# Определение используемой оболочки
+# Проверка используемой оболочки
 shell=$(basename "$SHELL")
 
-# Настройка Ruby Gems для выбранной оболочки
-declare -A shell_configure_functions=(
-    ["bash"]="configure_ruby_gems_bash"
-    ["fish"]="configure_ruby_gems_fish"
-    ["zsh"]="configure_ruby_gems_zsh"
-)
+# Установка Ruby для Arch Linux
+install_ruby_arch
 
-if [[ -n "${shell_configure_functions[$shell]}" ]]; then
-    ${shell_configure_functions[$shell]}
+# Настройка Ruby Gems для соответствующей оболочки
+if [[ "$shell" == "bash" ]]; then
+    configure_ruby_gems_bash
+elif [[ "$shell" == "fish" ]]; then
+    configure_ruby_gems_fish
+elif [[ "$shell" == "zsh" ]]; then
+    configure_ruby_gems_zsh
+else
+    echo "Не удалось определить используемую оболочку"
+    exit 1
 fi
 
 # Установка Jekyll и Bundler
